33a4e3eb11d73c364363be846abf4b5cf2f353ca
Lewi Belayneh Haile <horriblebob11@gmail.com>
2023-02-09 12:48:15 +0300

    added support for pdf_to_img.py and reformated the file to use functions

--- cli_run.js
+++ cli_run.js
@@ -1,13 +1,42 @@
-const { exec } = require("child_process");
+const { spawn, exec } = require("child_process");
 
-exec(" python img_to_txt.py 'imgs/161118 Letter_0.jpg'", (error, stdout, stderr) => {
-    if (error) {
+//execute pdf_to_img.py
+function pdf_to_img() {
+    const ls = spawn("python", ["pdf_to_img.py"]);
+
+    ls.stdout.on("data", data => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on("data", data => {
+        console.log(`stderr: ${data}`);
+    });
+
+    ls.on('error', (error) => {
         console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
+    });
+
+    ls.on("close", code => {
+        console.log(`child process exited with code ${code}`);
+        if(code === 0) {
+            img_to_txt()
+        }
+    });
+}
+
+function img_to_txt() {
+    //execute img_to_txt.py
+    exec(" python img_to_txt.py 'imgs/161118 Letter_0.jpg'", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+pdf_to_img()
