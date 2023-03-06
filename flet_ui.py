b19dab8536b3817d2a23d7ab82d938025ed2e859
Lewi Belayneh Haile <horriblebob11@gmail.com>
2023-03-06 10:05:48 +0300

    added dependecy directory detection and creation if not present

--- flet_ui.py
+++ flet_ui.py
@@ -1,15 +1,27 @@
+import os
 import pdf_to_img as pti
 import img_to_txt as itt
 import flet as ft
 
 
 def main(page: ft.Page):
+    paths = ['word', 'img', 'pdfs', 'raw']
+    for _path in paths:
+        if not os.path.exists(_path):
+            try:
+                os.mkdir(_path)
+            except FileExistsError:
+                pass
+
     page.title = "Beta OCR"
     page.theme = ft.Theme(color_scheme_seed='black', use_material3=True)
     page.window_width = 200
     page.window_height = 200
 
     def file_pick(e: ft.FilePickerResultEvent):
+        ocr_status.value = ""
+        ocr_status.update()
+
         selected_files.value = (
             ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled"
         )
@@ -30,13 +42,13 @@ def main(page: ft.Page):
 
                 itt.main(file[:-4])
                 print('Image converted to Text')
-                return "Complete"
+                return "Conversion Complete"
             elif file[-4:] in img_ext:
                 itt.main(file[:-4])
                 print('Image converted to Text')
-                return "Complete"
+                return "Conversion Complete"
             else:
-                return "Failed"
+                return "Conversion Failed"
 
         except ModuleNotFoundError:
             return "module Tesseract is not installed"
@@ -61,7 +73,8 @@ def main(page: ft.Page):
                 ),
                 selected_files,
                 ocr_status,
-            ]
+            ],
+            alignment=ft.alignment.center
         )
     )
 
